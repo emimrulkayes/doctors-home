9b673a5620d10a81e81b21f2ca69555c1864d7b6
emimrulkayes <emimrulkayes@gmail.com>
2022-08-04 23:43:40 +0600

    create Navbar and Banner

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './componants/Shared/Navbar';
+import { Routes, Route } from "react-router-dom";
+import About from './componants/Pages/About/About';
+import Home from './componants/Pages/Home/Home';
+import Contact from './componants/Pages/Contact/Contact';
+import Login from './componants/Pages/Login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path='/login' element={<Login />} />
+
+
+      </Routes>
     </div>
   );
 }
